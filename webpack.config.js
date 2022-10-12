fe71c3a8e7480c62768f60c0273933d831ce8359
Petr Martinek <petrklic2005@gmail.com>
2022-10-12 11:19:28 +0200

    webpack i & config, git config, json config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        main: path.resolve(_dirname, "src/index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        // filename: "[name].[contenthash].js",
+        // assetModuleFilename: "[name][ext]",
+        clean: true,
+    }
+}
